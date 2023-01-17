dc10361af468dddddd54b25cbef3ba27a1733da7
sora02 <leeseul1914@naver.com>
2023-01-17 11:10:49 +0900

    vue js 기본 파일

---
+++ portpolio/src/components/HeaderCom.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+      <nav>
+        <ul>
+          <li><a class="menu-btn" href="#name">프로필</a></li>
+          <li><a class="menu-btn" href="#cv">상세 내용</a></li>
+        </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderCom',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
